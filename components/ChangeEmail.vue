8877600e37db89c6ed48d40b82fb56e2ddd24478
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-09-11 16:46:33 +0100

    change password and email methods are complete, now I just have to test them and improve their layout

--- components/ChangeEmail.vue
+++ components/ChangeEmail.vue
@@ -1,13 +1,55 @@
 <template>
   <div>
-    <vs-input v-model="localEmail" primary placeholder="Insira o novo email" />
+    <vs-input v-model="changeEmail" primary placeholder="Insira o novo email" />
+    <vs-button primary @click="ChangeEmailFunc">
+      <i class="bx bx-refresh"></i>&nbsp; Alterar</vs-button
+    >
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    localEmail: '',
+    changeEmail: '',
   }),
+  methods: {
+    ChangeEmailFunc() {
+      const componentState = { email: this.changeEmail }
+      return (
+        this.$api
+          .put('/account/change_email', componentState, {
+            header: {
+              Authorization: `Bearer ${this.$store.state.auth.token}`,
+            },
+          })
+          .then(() => {
+            this.$vs.notification({
+              icon: `<i class='bx bx-check' ></i>`,
+              color: 'success',
+              progress: 'auto',
+              duration: 10000,
+              sticky: true,
+              position: 'top-right',
+              title: 'Email alterado com sucesso.',
+              text: `O novo email já se encontra em vigor.
+              Assim que iniciar uma nova sessão já o poderá utilizar.`,
+            })
+          })
+          // eslint-disable-next-line no-console
+          .catch(() => {
+            this.$vs.notification({
+              icon: `<i class='bx bx-error' ></i>`,
+              color: 'danger',
+              progress: 'auto',
+              duration: 10000,
+              sticky: true,
+              position: 'top-right',
+              title: 'Occorreu um erro.',
+              text: `Tem a certeza que preencheu os dados devidamente?`,
+            })
+          })
+      )
+    },
+  },
 }
 </script>
--- components/ChangePassword.vue
+++ components/ChangePassword.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <vs-input
-      v-model="localPassword"
+      v-model="changePassword"
       placeholder="Insira a nova palavra passe"
       primary
       type="password"
       :progress="getProgress"
       :visible-password="hasVisiblePassword"
       icon-after
-      @input="handleRegisterPassword"
       @click-icon="hasVisiblePassword = !hasVisiblePassword"
     >
       <template #icon>
@@ -20,6 +19,9 @@
         >Palavra passe segura</template
       >
     </vs-input>
+    <vs-button primary @click="ChangePasswordFunc">
+      <i class="bx bx-refresh"></i>&nbsp; Alterar</vs-button
+    >
   </div>
 </template>
 
@@ -27,30 +29,69 @@
 export default {
   data: () => ({
     hasVisiblePassword: false,
-    localPassword: '',
+    changePassword: '',
   }),
   computed: {
     getProgress() {
       let progress = 0
       // at least one number
-      if (/\d/.test(this.localPassword)) {
+      if (/\d/.test(this.changePassword)) {
         progress += 30
       }
       // at least one capital letter
-      if (/(.*[A-Z].*)/.test(this.localPassword)) {
+      if (/(.*[A-Z].*)/.test(this.changePassword)) {
         progress += 20
       }
       // at least a lowercase
-      if (/(.*[a-z].*)/.test(this.localPassword)) {
+      if (/(.*[a-z].*)/.test(this.changePassword)) {
         progress += 25
       }
       // more than 8 digits
-      if (this.localPassword === null) {
+      if (this.changePassword === null) {
         progress += 0
-      } else if (this.localPassword.length >= 8) {
+      } else if (this.changePassword.length >= 8) {
         progress += 25
       }
       return progress
+    },
+  },
+  methods: {
+    ChangePasswordFunc() {
+      const componentState = { password: this.changePassword }
+      return (
+        this.$api
+          .put('/account/change_password', componentState, {
+            header: {
+              Authorization: `Bearer ${this.$store.state.auth.token}`,
+            },
+          })
+          .then(() => {
+            this.$vs.notification({
+              icon: `<i class='bx bx-check' ></i>`,
+              color: 'success',
+              progress: 'auto',
+              duration: 10000,
+              sticky: true,
+              position: 'top-right',
+              title: 'Palavra passe alterada com sucesso.',
+              text: `A nova palavra passe já se encontra em vigor.
+              Assim que iniciar uma nova sessão já a poderá utilizar.`,
+            })
+          })
+          // eslint-disable-next-line no-console
+          .catch(() => {
+            this.$vs.notification({
+              icon: `<i class='bx bx-error' ></i>`,
+              color: 'danger',
+              progress: 'auto',
+              duration: 10000,
+              sticky: true,
+              position: 'top-right',
+              title: 'Occorreu um erro.',
+              text: `Tem a certeza que preencheu os dados devidamente?`,
+            })
+          })
+      )
     },
   },
 }
